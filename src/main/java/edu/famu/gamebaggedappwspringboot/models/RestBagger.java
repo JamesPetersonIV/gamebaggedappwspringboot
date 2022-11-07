0526a606314d1aaabc9c95301264f99af5481644
vacoote89 <vanessa.coote@gmail.com>
2022-11-06 23:11:43 -0500

    Add bagger DocumentReference object
    added missing variable and set method. get method still needed.

--- src/main/java/edu/famu/gamebaggedappwspringboot/models/RestBagger.java
+++ src/main/java/edu/famu/gamebaggedappwspringboot/models/RestBagger.java
@@ -6,9 +6,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 
+//TODO: You need to add the get method. I added the missing data member the set method.
 public class RestBagger extends AbstractBagger{
+    
+    private DocumentReference bagger; //added by V.C.
+    
     public RestBagger(String email, String uid, String firstName, String lastName) {
         super(email, uid, firstName, lastName); //calling parent constructor
+    }
+    //added by V.C.
+    public void setBagger(DocumentReference bagger) {
+        this.bagger = bagger;
     }
 
 }
